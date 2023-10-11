566f913123ea38745c3d1b7dbd593a2832e319cc
Said Gandur <ing.saidgandur@gmail.com>
2023-10-11 11:24:44 -0500

    Modyfing route strategy

--- src/app/components/location/location-details/location-details.component.ts
+++ src/app/components/location/location-details/location-details.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+import { ILocation } from 'src/app/utils/interfaces/location.interface';
 
 @Component({
   selector: 'app-location-details',
   templateUrl: './location-details.component.html',
   styleUrls: ['./location-details.component.scss']
 })
-export class LocationDetailsComponent implements OnInit {
-
-  constructor() { }
+export class LocationDetailsComponent implements OnInit, OnDestroy {
+  subscription: Subscription[] = [];
+  location!: ILocation;
+  constructor(
+    private rickAndMortyService: RickAndMortyService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getLocationId();
   }
 
+  private getLocationId(): void {
+    this.subscription.push(
+      this.route.paramMap.subscribe((params: ParamMap) => {
+        const id = params.get('id');
+        if (id) {
+          this.getLocationInfo(+id);
+        }
+      })
+    );
+  }
+
+  getLocationInfo(locationId: number): void {
+    this.subscription.push(
+      this.rickAndMortyService
+        .getLocations([locationId])
+        .subscribe(location => {
+          this.location = location as unknown as ILocation;
+          console.log({
+            location: this.location
+          });
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.map((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+  }
 }
--- src/app/components/location/location-page/location-page.component.ts
+++ src/app/components/location/location-page/location-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+import { ILocationResponse } from 'src/app/utils/interfaces/location.interface';
 
 @Component({
   selector: 'app-location-page',
   templateUrl: './location-page.component.html',
   styleUrls: ['./location-page.component.scss']
 })
-export class LocationPageComponent implements OnInit {
-
-  constructor() { }
+export class LocationPageComponent implements OnInit, OnDestroy {
+  subscription!: Subscription
+  locations!: ILocationResponse;
+  constructor(
+    private rickAndMortyService: RickAndMortyService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.initializeData();
+  }
+
+  private initializeData(): void {
+    this.subscription = this.rickAndMortyService
+      .getLocations()
+      .subscribe((res: ILocationResponse) => {
+      this.locations = res;
+      console.log({
+        locations: this.locations
+      });
+    })
+  }
+
+  goToLocationDetails(locationId: number): void {
+    this.router.navigate([`/location`, locationId])
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
